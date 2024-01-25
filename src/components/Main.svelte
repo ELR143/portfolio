f937a5dd4f0eb7495c62b6af02d2033e59ea2cc8
ELR143 <emmlou143@gmail.com>
2024-01-25 20:32:46 +0000

    set up components and page structure with filler text

---
+++ src/components/Main.svelte
@@ -0,0 +1,193 @@
+<script>
+  import Step from "./Step.svelte";
+
+  let steps = [
+    { name: "Treasure Hunters", icon: "fa-regular fa-gem" },
+    { name: "ELR-news", icon: "fa-regular fa-newspaper" },
+    { name: "ELR-news API", icon: "fa-solid fa-server" },
+  ];
+
+  let benefits = [
+    { name: "committed to learning", description: "i like lernin" },
+    { name: "a human", description: "i like beanz" },
+    { name: "hungry", description: "pls bring beans?" },
+  ];
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+  <section
+    id="introPage"
+    class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+  >
+    <div
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+    >
+      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi! I'm <span class="text-violet-400">Emma</span> Richardson <br />Full
+        Stack <span class="text-violet-400">Developer</span>
+      </h2>
+      <p class="text-base sm:text-lg md:text-xl">
+        My favourite tech includes Javascript (React, Next.js and Svelte),
+        TailwindCSS, Node.js + Express.js & PostgreSQL
+      </p>
+      <a
+        href="social media"
+        target="_blank"
+        class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+        ><div
+          class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+        ></div>
+        <h4 class="relative z-9">Get in touch &rarr;</h4></a
+      >
+    </div>
+    <div class="relative shadow-2xl grid place-items-center">
+      <img
+        src={""}
+        alt="Profile Image"
+        class="object-cover z-[2] max-h-[70vh]"
+      />
+    </div>
+  </section>
+
+  <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+    <div class="flex flex-col gap-2 text-center">
+      <h6 class="text-lg sm:text-xl md:text-2xl">
+        A few of my creative endeavours.
+      </h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        Curious to <span class="text-violet-400">see</span> my work?
+      </h3>
+    </div>
+    <a
+      href="rick roll lol"
+      target="_blank"
+      class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+    >
+      <i class="fa-regular fa-circle-play"> </i>
+      <p>Watch the video</p>
+    </a>
+    <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-10">
+      <Step step={steps[0]}>
+        <p>
+          Treasure Hunters is a GPS-based web application built using <strong
+            class="text-violet-400"
+            >Next.js, TailwindCSS, Django, Django REST</strong
+          >
+        </p>
+      </Step>
+      <Step step={steps[1]}>
+        <p>
+          ELR-news is a React CRUD application where users can view and comment
+          on articles <strong class="text-violet-400">React</strong>
+        </p>
+      </Step>
+      <Step step={steps[2]}>
+        <p>
+          ELR-news server is a RESTful API built using <strong
+            class="text-violet-400">Node.js & Express.js</strong
+          >
+        </p>
+      </Step>
+    </div>
+  </section>
+
+  <section
+    id="about"
+    class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+  >
+    <div
+      class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+    >
+      <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        A bit <span class="text-violet-400">about</span> me.
+      </h3>
+    </div>
+    <p class="mx-auto font-semibold text-lg sm:text-xl md:text-2xl">
+      I am . . .
+    </p>
+    <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+      {#each benefits as benefit, index}
+        <div class="flex gap-6 sm:gap-8">
+          <p
+            class="text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+          >
+            {index + 1}
+          </p>
+          <div class="flex flex-col gap-6 sm:gap-8">
+            <h3 class="text-2xl sm:text-3xl md:text-5xl">
+              {benefit.name}
+            </h3>
+            <p>{benefit.description}</p>
+          </div>
+        </div>
+      {/each}
+    </div>
+    <h5 class="text-2xl sm:3xl font-semibold text-center">
+      The <span class="text-violet-400">Complete</span> Package
+    </h5>
+    <div
+      class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
+    >
+      <table class="bg-white text-slate-700 rounded text-center">
+        <thead class="border-b border-solid border-slate-200">
+          <tr class="">
+            <th />
+            <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+            <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+            <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+            <th class="whitespace-nowrap bg-violet-700 text-white p-4 px-8"
+              >Me</th
+            >
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+              >Dedication</td
+            >
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
+          </tr>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+              >Critical thinking</td
+            >
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
+          </tr>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+              >Interpersonal skills</td
+            >
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
+          </tr>
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+              >Programming ability</td
+            >
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-slate-500" /></td>
+            <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+            <td><i class="fa-solid fa-check text-green-500" /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+      <p>Scroll to see more &rarr;</p>
+    </div>
+    <p class="mx-auto"></p>
+  </section>
+</main>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  import "../app.css";
+  import Header from "../components/Header.svelte";
+  import Footer from "../components/Footer.svelte";
+
+  let y;
+  let innerHeight = 0;
+  let innerWidth = 0;
+
+  function goToTop() {
+    document.body.scrollIntoView()
+  }
+</script>
+
+<div
+  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+>
+  <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[100] " + (y > 0 ? 'opacity-full pointer-events-auto' : 'opacity-0 pointer-events-none')}>
+    <button on:click={goToTop} class="ml-auto rounded-full aspect-square bg-slate-950 text-violet-400 px-3 sm:px-4 hovering:bg-slate-800 cursor-pointer">
+      <i class="fa-solid fa-arrow-up grid place-items-center aspect-square"></i>
+    </button>
+  </div>
+  <Header y={y}/>
+  <slot />
+  <Footer />
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
